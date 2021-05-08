4e2101e717908b7087ef42403c46ca4e38b66a9a
DamianMoris <61407816+DamianMoris@users.noreply.github.com>
2021-05-08 17:27:36 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,40 +1,37 @@
-#include <stdio.h>
-#include <string.h>
-#include <zmq.h>
-
-void * context; //Global context, because you only need one !
-
-int main( int argc, char * argv[] )
-{
-	context = zmq_ctx_new();
-
-    void * pusher = zmq_socket(context, ZMQ_PUSH);
-    void * subscriber = zmq_socket(context, ZMQ_SUB);
-
-    zmq_connect(pusher, "tcp://benternet.pxl-ea-ict.be:24041");
-    zmq_connect(subscriber, "tcp://benternet.pxl-ea-ict.be:24042");
-
-    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "example>task? >", strlen("example>task? >"));
-
-    for(int request_nbr = 0; request_nbr != 10; request_nbr++)
-    {
-        printf("Test%d\n",request_nbr);
-        char buffer[10];
-        printf("Test12\n");
-        //printf ("Sending Hello");
-        zmq_send(pusher, "Worp", 4, 0);
-        zmq_recv(subscriber, buffer, 10, 0);printf("Test2\n");
-        printf("Worp");printf("Test%d\n",request_nbr);
-    }
-
-    zmq_send(pusher, "Damian Moris", 12, 0);
-
-	//ALL MAGIC HAPPENS HERE
-	printf("Magic!\n");
-
-    zmq_close(pusher);
-    zmq_ctx_shutdown(context); //optional for cleaning lady order (get ready you l*zy f*ck)
-    zmq_ctx_term(context); //cleaning lady goes to work
-	printf("KTNXBYE!\n");
-	return 0;
-}
+//#include <stdio.h>
+//#include <string.h>
+//#include <zmq.h>
+//
+//void * context; //Global context, because you only need one !
+//
+//int main( int argc, char * argv[] )
+//{
+//    printf("Start\n");
+//    void * context = zmq_ctx_new();
+//
+//    void * pusher = zmq_socket(context, ZMQ_PUSH);
+//    void * subscriber = zmq_socket(context, ZMQ_SUB);
+//
+//    zmq_connect(pusher, "tcp://benternet.pxl-ea-ict.be:24041");
+//    zmq_connect(subscriber, "tcp://benternet.pxl-ea-ict.be:24042");
+//
+//    //zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "example>task? >", strlen("example>task? >"));
+//
+//    for(int request_nbr = 0; request_nbr != 1; request_nbr++)
+//    {
+//        char buffer[10];
+//        zmq_send(pusher, "Worp", 4, 0);
+//        zmq_recv(pusher, buffer, 10, 0);
+//    }
+//
+//    //zmq_send(pusher, "Damian Moris", 12, 0);
+//
+//	//ALL MAGIC HAPPENS HERE
+//	printf("Magic!\n");
+//
+//    zmq_close(pusher);
+//    zmq_ctx_shutdown(context); //optional for cleaning lady order (get ready you l*zy f*ck)
+//    zmq_ctx_term(context); //cleaning lady goes to work
+//	printf("KTNXBYE!\n");
+//	return 0;
+//}
--- service.c
+++ service.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int service()
+unsigned int coinflip();
+unsigned int rng();
+unsigned int d4();
+unsigned int d6();
+unsigned int d8();
+unsigned int d12();
+unsigned int d20();
+unsigned int d100();
+int wheelspin();
+
+int main()
 {
+    srand(time(NULL));
+
+    wheelspin();
+
+    //for(int i = 0; i < 50; i++)
+    //{
+    //    printf("%d, %d, %d, %d, %d, %d \n", d4(), d6(), d8(), d12(), d20(), d100());
+    //}
+
+        //printf("d4: %d\nd6: %d\nd8: %d\nd12: %d\nd20: %d\nd100: %d\n", d4(), d6(), d8(), d12(), d20(), d100());
 
     return 0;
+}
+
+unsigned int coinflip()             {return (rand() % 2);}
+unsigned int rng(unsigned int i)    {return (rand() % (i + 1));}
+unsigned int d4()                   {return (rand() % 4 + 1);}
+unsigned int d6()                   {return (rand() % 6 + 1);}
+unsigned int d8()                   {return (rand() % 8 + 1);}
+unsigned int d12()                  {return (rand() % 12 + 1);}
+unsigned int d20()                  {return (rand() % 20 + 1);}
+unsigned int d100()                 {return (rand() % 100 + 1);}
+
+int wheelspin()
+{
+    char* array[10] = {"1","22","333","4444"};
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf("Enter a word\n");
+        scanf("%s", array[i]);
+    }
+
+    printf("wheelspin\n");
+    array[0] = "azertyop";
+    printf("array[random]: %s\n", array[rand() % 4]); //
+
+    return (1);
 }
